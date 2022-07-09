bd0b2367432b9913733fd6e866ff163d7f946795
Roman <ipromanone@gmail.com>
2022-07-09 20:34:06 +0300

    connected plugin body-parser finalized js code

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express') // подключение плагина exspress
+const express = require('express') // подключение плагина exspress server
+const bodyParser = require('body-parser') //  подключение плагина body-parser
 
 const app = express() // вызов 
 
 app.set('view engine','ejs') // настройка плагина ejs
 app.use(express.static('public')) // указываем статический путь (подключение стилей)
+app.use(bodyParser.urlencoded({extended: true})) // настройка плагина body-parser
 
 app.get('/', (request,response) => {
     response.render('index')  // подключение веб-страницы
+})
+app.post('/', (request,response) => { // обработка POST запроса
+    const { city } = request.body
+
+    console.log(city)
+    response.render('index')
 })
 
 app.listen(3000, ()=> {
