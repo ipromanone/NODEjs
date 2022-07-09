b37702f0728467e35bd06538f72c088e05196901
Roman <ipromanone@gmail.com>
2022-07-09 22:13:07 +0300

    getting weather via api

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express') // подключение плагина exspress server
 const bodyParser = require('body-parser') //  подключение плагина body-parser
-const weatherRequest = require('./requests/weather.request')
-
-// 69a59655bbe4d5fc82a38bc6f87eb48e
+const weatherRequest = require('./requests/weather.request') // Подключаем модуль из файла weather.request.js
 
 const app = express() // вызов 
 
@@ -10,13 +8,15 @@ app.set('view engine','ejs') // настройка плагина ejs
 app.use(express.static('public')) // указываем статический путь (подключение стилей)
 app.use(bodyParser.urlencoded({extended: true})) // настройка плагина body-parser
 
-app.get('/', (request,response) => {
+app.get('/',(request,response) => {
     response.render('index')  // подключение веб-страницы
 })
-app.post('/', (request,response) => { // обработка POST запроса
+app.post('/', async (request,response) => { // обработка POST запроса
     const { city } = request.body
 
-    weatherRequest(city)
+   const {weather, error} = await weatherRequest(city)
+   console.log('Weather', weather);
+   console.log('Error', error)
     response.render('index')
 })
 
--- requests/weather.request.js
+++ requests/weather.request.js
@@ -4,5 +4,34 @@ module.exports = async function(city = '') {
     if(!city) {
         throw new Error('Имя города не может быть пустым')
     }
-    console.log('City:', city)
+
+    const KEY = '69a59655bbe4d5fc82a38bc6f87eb48e'
+    const uri = `https://api.openweathermap.org/data/2.5/weather`
+    
+    const options = {
+        uri,
+        qs: {
+            appid: KEY,
+            q: city,
+            units: 'imperial',
+        },
+        json: true
+    }
+
+    try {
+        const data = await rp(options)
+        const celsius = (data.main.temp - 32) * 5/9 // Формула келвины переводим в целсии
+        return {
+            weather: `${data.name} ${celsius.toFixed(0)}`,
+            error: null
+        }
+    } catch (error) {
+        return {
+            weather: null,
+            error: error.error.message
+        }
+    }
+
+
+
 }
